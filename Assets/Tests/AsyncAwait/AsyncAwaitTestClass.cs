b69de81049e04f5cba8677f212b7642dea2d184f
HarumiMaruoka <106292983+HarumiMaruoka@users.noreply.github.com>
2022-10-14 00:16:41 +0900

    [add] async/awaitの練習の記録

---
+++ Assets/Tests/AsyncAwait/AsyncAwaitTestClass.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Threading;
+using System.Threading.Tasks;
+
+[System.Serializable]
+public class AsyncAwaitTestClass
+{
+    /// <summary>
+    /// 別スレッドで実行するメソッド。
+    /// </summary>
+    /// <returns>
+    /// 非同期メソッドには、次の戻り値の型があります。
+    /// Task:          操作を実行し、値を返さない非同期メソッドの場合。
+    /// Task<TResult>: 値を返す非同期メソッドの場合。
+    /// </returns>
+    public async Task Test()
+    {
+        var result = await ExampleAsync();
+        Debug.Log(result); // 1
+    }
+
+    /// <summary>
+    /// awaitで非同期で処理を待ちたいメソッド。
+    /// Genericで型を指定する。
+    /// </summary>
+    /// <returns>
+    /// 戻り値の型
+    /// </returns>
+    public async Task<int> ExampleAsync()
+    {
+        // 1秒待つ
+        Debug.Log("始まり");
+        await Task.Run(() => Thread.Sleep(5000));
+        // Genericで指定した型の値を返す。
+        Debug.Log("終わり");
+        return 1;
+    }
+}
---
+++ Assets/Tests/AsyncAwait/AsyncAwaitTestComponent.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Threading;
+using System.Threading.Tasks;
+
+/// <summary>
+/// async/await勉強用コンポーネント
+/// </summary>
+public class AsyncAwaitTestComponent : MonoBehaviour
+{
+    [SerializeField]
+    int _waitTime = 1000;
+    [SerializeField]
+    KeyCode _runKeyCode;
+
+
+    AsyncAwaitTestClass _asyncAwaitTestClass = new AsyncAwaitTestClass();
+    /// <summary>
+    /// Startを別スレッドで実行する。
+    /// </summary>
+    /// <returns>
+    /// 非同期メソッドには、次の戻り値の型があります。
+    /// Task:          操作を実行し、値を返さない非同期メソッドの場合。
+    /// Task<TResult>: 値を返す非同期メソッドの場合。
+    /// </returns>
+    private async Task Start()
+    {
+        var result = await ExampleAsync();
+        Debug.Log(result); // 1
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(_runKeyCode))
+        {
+            var a = _asyncAwaitTestClass.ExampleAsync();
+        }
+    }
+
+    /// <summary>
+    /// awaitで非同期で処理を待ちたいメソッド。
+    /// Genericで型を指定する。
+    /// </summary>
+    /// <returns>
+    /// 戻り値の型
+    /// </returns>
+    private async Task<int> ExampleAsync()
+    {
+        // 1秒待つ
+        await Task.Run(() => Thread.Sleep(_waitTime));
+        // Genericで指定した型の値を返す。
+        return 1;
+    }
+}
